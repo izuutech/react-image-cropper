eee09f36733ff8128a3ba641765eaaefb70801d6
izuutech <izutechsofficial@gmail.com>
2023-01-11 17:00:09 +0100

    start cropping

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import ReactCrop from "react-image-crop";
 import "react-image-crop/dist/ReactCrop.css";
@@ -7,22 +7,21 @@ import "./App.css";
 import { Modal } from "antd";
 
 function App() {
-  const [count, setCount] = useState(0);
   const [src, setSrc] = useState("");
   const [openModal, setOpenModal] = useState(false);
+  const imgRef = useRef(null);
+
+  const [result, setResult] = useState(null);
 
   const [crop, setCrop] = useState({
-    unit: "%", // Can be 'px' or '%'
+    unit: "px", // Can be 'px' or '%'
     x: 25,
     y: 25,
-    width: 50,
-    height: 50,
+    width: 100,
+    height: 100,
   });
 
-  useEffect(() => {
-    console.log(crop);
-    return () => {};
-  }, [crop]);
+  const [completedCrop, setCompletedCrop] = useState({});
 
   const toggleModal = () => {
     setOpenModal(!openModal);
@@ -38,7 +37,41 @@ function App() {
     };
   };
 
-  const cropImg = () => {};
+  const cropImg = async () => {
+    const image = imgRef.current;
+    console.log(crop, "ll");
+    console.log(completedCrop, "v");
+
+    //if the person has not touched to crop, use default cropping value
+    const theCrop = completedCrop.width ? completedCrop : crop;
+
+    try {
+      const canvas = document.createElement("canvas");
+      const scaleX = image.naturalWidth / image.width;
+      const scaleY = image.naturalHeight / image.height;
+      canvas.width = theCrop.width;
+      canvas.height = theCrop.height;
+      const ctx = canvas.getContext("2d");
+      ctx.drawImage(
+        image,
+        theCrop.x * scaleX,
+        theCrop.y * scaleY,
+        theCrop.width * scaleX,
+        theCrop.height * scaleY,
+        0,
+        0,
+        theCrop.width,
+        theCrop.height
+      );
+
+      const base64Image = canvas.toDataURL("image/jpeg", 1);
+
+      setResult(base64Image);
+      toggleModal();
+    } catch (e) {
+      console.log("crop the image");
+    }
+  };
 
   return (
     <div className="w-[100%]">
@@ -51,8 +84,15 @@ function App() {
         width={400}
         footer={null}
       >
-        <ReactCrop crop={crop} onChange={(c) => setCrop(c)}>
-          <img src={src} />
+        <ReactCrop
+          crop={crop}
+          onChange={(c) => setCrop(c)}
+          onComplete={(c) => {
+            setCompletedCrop(c);
+          }}
+          keepSelection={true}
+        >
+          <img src={src} ref={imgRef} />
         </ReactCrop>
         <button onClick={cropImg} className="bg-zinc-400 text-white">
           Crop
@@ -70,7 +110,7 @@ function App() {
 
       <div className="mt-10">
         <span>Output</span>
-        <img src={"src.png"} alt="output" />
+        <img src={result} alt="output" />
       </div>
     </div>
   );
