7f176f38ebb5939b78d3c9830b84696cb486b6db
izuutech <izutechsofficial@gmail.com>
2023-01-11 13:59:16 +0100

    start opening crop

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from "./assets/react.svg";
 import ReactCrop from "react-image-crop";
 import "react-image-crop/dist/ReactCrop.css";
 import "./App.css";
-import src from "../public/userOne.png";
+
 import { Modal } from "antd";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [src, setSrc] = useState("");
+  const [openModal, setOpenModal] = useState(false);
 
   const [crop, setCrop] = useState({
     unit: "%", // Can be 'px' or '%'
@@ -22,15 +24,30 @@ function App() {
     return () => {};
   }, [crop]);
 
+  const toggleModal = () => {
+    setOpenModal(!openModal);
+  };
+
+  const selectImage = (e) => {
+    const theImg = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(theImg);
+    reader.onload = () => {
+      setSrc(reader.result);
+      toggleModal();
+    };
+  };
+
   const cropImg = () => {};
+
   return (
     <div className="w-[100%]">
       <Modal
         title={`Upload image`}
-        visible={true}
+        visible={openModal}
         className="font-body"
         // closable={false}
-
+        onCancel={toggleModal}
         width={400}
         footer={null}
       >
@@ -41,14 +58,19 @@ function App() {
           Crop
         </button>
       </Modal>
-      <span>saaaaap</span>
+      <span>Select Image to Crop</span>
       <div>
-        <input type="file" className="border p-2" placeholder="Add image" />
+        <input
+          type="file"
+          className="border p-2"
+          placeholder="Add image"
+          onChange={selectImage}
+        />
       </div>
 
-      <div>
+      <div className="mt-10">
         <span>Output</span>
-        <img src={src} />
+        <img src={"src.png"} alt="output" />
       </div>
     </div>
   );
